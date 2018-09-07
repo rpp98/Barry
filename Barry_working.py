1798ed5cd7cb43e9e23db18f0bf8b8cc86569209
rpp98 <40677087+rpp98@users.noreply.github.com>
2018-09-07 13:20:10 -0400

    Update Barry_working.py

--- Barry_working.py
+++ Barry_working.py
@@ -203,6 +203,7 @@ async def recent(ctx):
     embed2 = discord.Embed(title='Recent Triple Divergences for all Time Frames',description='')
     tf_converter = {'1h':'1 Hour', '2h':'2 Hour', '4h':'4 Hour', '6h':'6 Hour', '8h':'8 Hour', '12h':'12 Hour', '1d':'1 Day'}
     field_counter = 0
+    char_counter = []
     for time_frame in time_frames:
         results_dict = bot.results_dict
         results_fr,results_cd = results_dict[time_frame]
@@ -217,9 +218,12 @@ async def recent(ctx):
                     field_counter += 1
                     if header != 'None' or body != 'None':
                         print(header,body)
+                        char_counter.append(len(header) + len(body))
                         embed2.add_field(name=header,value=body)
                         await asyncio.sleep(0.05)
     print(field_counter)
+    print(char_counter)
+    print(sum(char_counter))
     await bot.say(embed=embed2)
 
 @bot.command(pass_context=True)
